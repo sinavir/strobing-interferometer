32ba63762dd3de6465e63247a6bb96411a163100
sinavir <sinavir@sinavir.fr>
2024-03-14 10:57:16 +0100

    init C version

---
+++ playground_thorlab_sdk/shell.nix
@@ -0,0 +1,9 @@
+{ nixpkgs ? (import ../npins).nixpkgs, pkgs ? import nixpkgs {} }:
+pkgs.mkShell {
+  packages = [
+  ];
+  shellHook = ''
+    # fixes libstdc++ issues and libgl.so issues
+    LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib/:${pkgs.udev}/lib:${builtins.toString ./prebuilt}
+  '';
+}
